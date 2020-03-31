fe2181a6a8277dcb07bc437dbf4accda0d0d51b1
Laurent DEBEAUJON <Laurent.DEBEAUJON@gmail.com>
2020-03-31 19:33:20 +0200

    new organisation

---
+++ Instagrid/Views/Layout1View.swift
@@ -0,0 +1,21 @@
+//
+//  Layout1View.swift
+//  Instagrid
+//
+//  Created by Laurent Debeaujon on 31/03/2020.
+//  Copyright © 2020 Laurent Debeaujon. All rights reserved.
+//
+
+import SwiftUI
+
+struct Layout1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Layout1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Layout1View()
+    }
+}
