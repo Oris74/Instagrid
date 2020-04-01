a0b105a43c6e04a5746b012c5c0b892e9c67a388
Laurent DEBEAUJON <Laurent.DEBEAUJON@gmail.com>
2020-04-01 14:06:49 +0200

    with 3 files Views

---
+++ Instagrid/Layout2View.swift
@@ -0,0 +1,21 @@
+//
+//  Layout2View.swift
+//  Instagrid
+//
+//  Created by Laurent Debeaujon on 01/04/2020.
+//  Copyright © 2020 Laurent Debeaujon. All rights reserved.
+//
+
+import UIKit
+
+class Layout2View: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
