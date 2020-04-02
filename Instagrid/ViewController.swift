0afd374456e50450768300d4a18df458992ea9b8
Laurent DEBEAUJON <Laurent.DEBEAUJON@gmail.com>
2020-04-02 23:36:28 +0200

    functional buttons

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -10,26 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
-        @IBOutlet weak var swipeDisplayPattern: UIView!
+     
+   @IBOutlet weak var displayPattern: UIView!
+    
+   @IBOutlet weak var buttonPortraitLeft: UIButton!
+   @IBOutlet weak var buttonPortraitRight: UIButton!
+   @IBOutlet weak var buttonPortraitCentral: UIButton!
+    
+    
+   @IBOutlet weak var buttonLandscapeUp: UIButton!
+   @IBOutlet weak var buttonLandscapeMiddle: UIButton!
+   @IBOutlet weak var buttonLandscapeDown: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-    @IBAction func buttonPortraitLeft(_ sender: Any) {
+    @IBAction func didTouchButtonPortraitLeft(_ sender: Any) {
+        buttonPortraitLeft.setImage(UIImage(named:"Selected.png"), for: .normal)
+        buttonPortraitCentral.setImage(nil, for: .normal)
+        buttonPortraitRight.setImage(nil, for: .normal)
+        
     }
     
-    @IBAction func buttonPortraitCentral(_ sender: Any) {
-    }
-    @IBAction func buttonPortraitRight(_ sender: Any) {
+    @IBAction func didTouchButtonPortraitCentral(_ sender: Any) {
+        buttonPortraitLeft.setImage(nil, for: .normal)
+        buttonPortraitCentral.setImage(UIImage(named: "Selected.png"), for: .normal)
+        buttonPortraitRight.setImage(nil, for: .normal)
+        
     }
     
-    @IBAction func ButtonLandscapeUp(_ sender: Any) {
+    @IBAction func didTouchButtonPortraitRight(_ sender: Any) {
+        buttonPortraitLeft.setImage(nil, for: .normal)
+        buttonPortraitCentral.setImage(nil, for: .normal)
+        buttonPortraitRight.setImage(UIImage(named:"Selected.png"), for: .normal)
+        
     }
-    @IBAction func buttonLandscapeMiddle(_ sender: Any) {
+    
+    @IBAction func didTouchButtonLandscapeUp(_ sender: Any) {
+        buttonLandscapeUp.setImage(UIImage(named:"Selected.png"), for: .normal)
+        buttonLandscapeMiddle.setImage(nil, for: .normal)
+        buttonLandscapeDown.setImage(nil, for: .normal)
+        
     }
-    @IBAction func buttonLandscapeDown(_ sender: Any) {
+    @IBAction func didTouchButtonLandscapeMiddle(_ sender: Any) {
+        buttonLandscapeUp.setImage(nil, for: .normal)
+        buttonLandscapeMiddle.setImage(UIImage(named:"Selected.png"), for: .normal)
+        buttonLandscapeDown.setImage(nil, for: .normal)
+        
+    }
+    @IBAction func didTouchButtonLandscapeDown(_ sender: Any) {
+        buttonLandscapeUp.setImage(nil, for: .normal)
+        buttonLandscapeMiddle.setImage(nil, for: .normal)
+        buttonLandscapeDown.setImage(UIImage(named:"Selected.png"), for: .normal)
+        
     }
     
     
